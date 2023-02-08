6e3c9ccabc5bc7975a15fbc71b1690f1b5334938
Roy <royhermens@hotmail.com>
2023-02-08 18:17:22 +0100

    Async swapping

--- i3swap.py
+++ i3swap.py
@@ -1,4 +1,5 @@
-#!python3 
+#!/usr/bin/python3 
+
 from i3ipc.aio import Connection 
 from dataclasses import dataclass
 from i3ipc.replies import WorkspaceReply
@@ -18,9 +19,11 @@ async def main():
         secondary=next(ws for ws in filteredWorkspaces if not ws.ipc_data.get("focused"))
     )
 
-    await i3.command(f"[workspace={workspaces.primary.ipc_data.get('num')}] move workspace to output {workspaces.secondary.ipc_data.get('output')}")
-    await i3.command(f"[workspace={workspaces.secondary.ipc_data.get('num')}] move workspace to output {workspaces.primary.ipc_data.get('output')}")
-    await i3.command(f"[workspace={workspaces.secondary.ipc_data.get('num')}] focus")
+    await asyncio.gather(
+        i3.command(f"[workspace={workspaces.primary.ipc_data.get('num')}] move workspace to output {workspaces.secondary.ipc_data.get('output')}"),
+        i3.command(f"[workspace={workspaces.secondary.ipc_data.get('num')}] move workspace to output {workspaces.primary.ipc_data.get('output')}"),
+        i3.command(f"[workspace={workspaces.secondary.ipc_data.get('num')}] focus")
+    )
 
 if __name__ == "__main__":
     import asyncio
