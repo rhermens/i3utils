2eb517cc1bc92b9a8c6cc71296b039e403793257
Roy <royhermens@hotmail.com>
2023-02-22 20:20:00 +0100

    Korter

--- i3masterstack.py
+++ i3masterstack.py
@@ -3,7 +3,7 @@
 from i3ipc.aio import Connection 
 import asyncio
 
-from i3ipc.events import Event, IpcBaseEvent, WindowEvent
+from i3ipc.events import Event
 
 async def set_split_direction(conn: Connection):
     focused = (await conn.get_tree()).find_focused()
@@ -19,14 +19,11 @@ async def set_split_direction(conn: Connection):
     if len(ws.nodes) > 1 and ws.nodes[1].layout != "splitv":
         await conn.command(f"[con_id={ws.nodes[1].id}] split v")
 
-def on_event(conn: Connection, event: IpcBaseEvent):
-    asyncio.get_event_loop().create_task(set_split_direction(conn))
-
 async def main():
     i3 = await Connection().connect()
 
-    i3.on(Event.WINDOW_NEW, on_event)
-    i3.on(Event.WINDOW_CLOSE, on_event)
+    i3.on(Event.WINDOW_NEW, lambda conn, _: None if asyncio.get_event_loop().create_task(set_split_direction(conn)) else None)
+    i3.on(Event.WINDOW_CLOSE, lambda conn, _: None if asyncio.get_event_loop().create_task(set_split_direction(conn)) else None)
 
     await i3.main()
 
