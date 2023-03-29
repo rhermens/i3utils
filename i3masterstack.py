cb72b861a0ff597d327a70a6b958a1c79f2e2303
Roy <royhermens@hotmail.com>
2023-03-29 08:12:48 +0200

    Marks

--- i3masterstack.py
+++ i3masterstack.py
@@ -14,16 +14,27 @@ async def set_split_direction(conn: Connection):
     if ws is None:
         return;
 
+    print(len(ws.nodes))
+
     if len(ws.nodes) == 1 and ws.nodes[0].layout != "splith":
+        print("splitting h")
         await conn.command(f"[con_id={ws.nodes[0].id}] split h")
     if len(ws.nodes) > 1 and ws.nodes[1].layout != "splitv":
+        print("splitting v")
         await conn.command(f"[con_id={ws.nodes[1].id}] split v")
+    if len(ws.nodes) > 2:
+        commands = []
+        target = ws.nodes[1].nodes[len(ws.nodes[1].nodes) - 1]
+        for node in ws.nodes[2:]:
+            commands.append(conn.command(f"[con_id={target.id}] mark {target.id}; [con_id={node.id}] move to mark {target.id}; [con_id={target.id}] unmark;"))
+        await asyncio.gather(*commands)
 
 async def main():
     i3 = await Connection().connect()
 
     i3.on(Event.WINDOW_NEW, lambda conn, _: None if asyncio.get_event_loop().create_task(set_split_direction(conn)) else None)
     i3.on(Event.WINDOW_CLOSE, lambda conn, _: None if asyncio.get_event_loop().create_task(set_split_direction(conn)) else None)
+    i3.on(Event.WINDOW_MOVE, lambda conn, _: None if asyncio.get_event_loop().create_task(set_split_direction(conn)) else None)
 
     await i3.main()
 
